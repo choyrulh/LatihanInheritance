138bb9e109dce5795a7b4757958ed701dbf0120f
choyrulh <choirul.humam@students.amikom.ac.id>
2020-04-11 19:27:18 +0700

    Latihan Inheritance

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Choirul", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Humam", 30, "191128", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Aveiro", 19, "2797", "choirul.humam@students.amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
